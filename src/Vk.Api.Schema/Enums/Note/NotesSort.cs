7d64fd6366179438e145877bf312d2dd711fbdec
Valentin447 <valentin@myttk.ru>
2017-10-09 04:47:01 +0700

    Добавил перечисление определяющее как сортировать список заметок

---
+++ src/Vk.Api.Schema/Enums/Note/NotesSort.cs
@@ -0,0 +1,18 @@
+﻿namespace Vk.Api.Schema.Enums.Note
+{
+    /// <summary>
+    /// Определяет как сортировать список заметок
+    /// </summary>
+    public enum NotesSort
+    {
+        /// <summary>
+        /// По дате создания в порядке убывания
+        /// </summary>
+        Descending,
+
+        /// <summary>
+        /// По дате создания в порядке возрастания
+        /// </summary>
+        Ascending
+    }
+}
