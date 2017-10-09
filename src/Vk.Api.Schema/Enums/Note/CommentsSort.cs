da67139691672bc630b2214779bf8639150940f8
Valentin447 <valentin@myttk.ru>
2017-10-09 22:51:32 +0700

    Добавил перечисление, определяющее как сортировать список комментариев к заметке

---
+++ src/Vk.Api.Schema/Enums/Note/CommentsSort.cs
@@ -0,0 +1,18 @@
+﻿namespace Vk.Api.Schema.Enums.Note
+{
+    /// <summary>
+    /// Определяет как сортировать список комментариев к заметке
+    /// </summary>
+    public enum CommentsSort
+    {
+        /// <summary>
+        /// По дате добавления в порядке возрастания
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// По дате добавления в порядке убывания
+        /// </summary>
+        Descending
+    }
+}
