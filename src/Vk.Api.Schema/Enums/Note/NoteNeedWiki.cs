fe92d437a78d38ea6ebf2f06619dbdaca1d692b1
Valentin447 <valentin@myttk.ru>
2017-10-09 22:09:36 +0700

    Добавил перечисление определяющее требуется ли в ответе wiki-представление заметки

---
+++ src/Vk.Api.Schema/Enums/Note/NoteNeedWiki.cs
@@ -0,0 +1,18 @@
+﻿namespace Vk.Api.Schema.Enums.Note
+{
+    /// <summary>
+    /// Определяет, требуется ли в ответе wiki-представление заметки 
+    /// </summary>
+    public enum NoteNeedWiki
+    {
+        /// <summary>
+        /// Не требуется wiki-представление заметки
+        /// </summary>
+        NotNeed,
+
+        /// <summary>
+        /// Требуется wiki-представление заметки
+        /// </summary>
+        Need
+    }
+}
