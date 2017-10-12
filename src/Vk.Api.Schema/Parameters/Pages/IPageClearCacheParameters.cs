021af610cb5e61e72e3cca6e7b75cf1010499346
Valentin447 <valentin@myttk.ru>
2017-10-12 21:13:08 +0700

    Добавил интерфейс для представления параметров запроса для очистки кеша внешних страниц

---
+++ src/Vk.Api.Schema/Parameters/Pages/IPageClearCacheParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace Vk.Api.Schema.Parameters.Pages
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для pages.clearCache <para/>
+    /// Документация: <see href="https://vk.com/dev/pages.clearCache"/>
+    /// </summary>
+    public interface IPageClearCacheParameters
+    {
+        /// <summary>
+        /// Адрес страницы, закешированную версию которой необходимо очистить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        string Url { get; set; }
+    }
+}
