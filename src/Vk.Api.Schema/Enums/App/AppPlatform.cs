ddff4b736dd5cf3bdbe6fceade99706ce2acd7fc
Valentin447 <valentin@myttk.ru>
2017-10-19 08:05:47 +0700

    Добавил перечисления возможных платформ

---
+++ src/Vk.Api.Schema/Enums/App/AppPlatform.cs
@@ -0,0 +1,28 @@
+﻿namespace Vk.Api.Schema.Enums.App
+{
+    /// <summary>
+    /// Платформа
+    /// </summary>
+    public enum AppPlatform
+    {
+        /// <summary>
+        /// iOS
+        /// </summary>
+        ios,
+
+        /// <summary>
+        /// Android
+        /// </summary>
+        android,
+
+        /// <summary>
+        /// Windows Phone
+        /// </summary>
+        winphone,
+
+        /// <summary>
+        /// Приложения на vk.com
+        /// </summary>
+        web
+    }
+}
