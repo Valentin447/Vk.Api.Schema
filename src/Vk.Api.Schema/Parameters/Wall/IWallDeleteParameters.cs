968eabd6146d2d123d25d09f7d41c41ad7246a03
Valentin447 <valentin@myttk.ru>
2017-10-27 13:25:38 +0700

    Добавил интерфейс для представления параметров запроса, удаляющего запись со стены

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallDeleteParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.delete <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.delete"/>
+    /// </summary>
+    public interface IWallDeleteParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, на стене которого находится запись
+        /// </summary>
+        /// <remarks>
+        ///  идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        ///  — например, OwnerId=-1 
+        /// По умолчанию идентификатор текущего пользователя
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор записи на стене
+        /// </summary>
+        int? PostId { get; set; }
+    }
+}
