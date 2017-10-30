759b1a6647fe967fb71cd5387d231eefe585ae1e
Valentin447 <valentin@myttk.ru>
2017-10-30 14:37:32 +0700

    Добавил перечисление и интерфейс для представления параметров запроса, возвращающего список записей со стены пользователя или сообщества

---
+++ src/Vk.Api.Schema/Enums/Wall/TypePostOnWall.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Vk.Api.Schema.Enums.Wall
+{
+    /// <summary>
+    /// Типы записей на стене
+    /// </summary>
+    public enum TypePostOnWall
+    {
+        /// <summary>
+        /// Предложенные записи на стене сообщества (доступно только при вызове с передачей AccessToken)
+        /// </summary>
+        [Description("suggests")]
+        Suggests,
+
+        /// <summary>
+        /// Отложенные записи (доступно только при вызове с передачей AccessToken)
+        /// </summary>
+        [Description("postponed")]
+        Postponed,
+
+        /// <summary>
+        /// Записи владельца стены
+        /// </summary>
+        [Description("owner")]
+        Owner,
+
+        /// <summary>
+        /// Записи не от владельца стены
+        /// </summary>
+        [Description("others")]
+        Others,
+
+        /// <summary>
+        /// Все записи на стене (owner + others)
+        /// </summary>
+        [Description("all")]
+        All
+    }
+}
---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallGetParameters.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Vk.Api.Schema.Enums.Wall;
+
+namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.get <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.get"/>
+    /// </summary>
+    public interface IWallGetParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, со стены которого необходимо получить записи
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию — текущий пользователь
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Короткий адрес пользователя или сообщества
+        /// </summary>
+        string Domain { get; set; }
+
+        /// <summary>
+        /// Смещение, необходимое для выборки определенного подмножества записей
+        /// </summary>
+        int? Offset { get; set; }
+
+        /// <summary>
+        /// Количество записей, которое необходимо получить
+        /// </summary>
+        /// <remarks>
+        /// Максимальное значение: 100
+        /// </remarks>
+        int? Count { get; set; }
+
+        /// <summary>
+        /// Типы записей на стене, которые необходимо получить
+        /// </summary>
+        TypePostOnWall? Filter { get; set; }
+
+        /// <summary>
+        /// 1 - Возвращать в ответе дополнительные поля profiles и groups, 
+        /// содержащие информацию о пользователях и сообществах
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию: 0
+        /// </remarks>
+        bool? IsExtended { get; set; }
+
+        /// <summary>
+        /// Список дополнительных полей для профилей и групп, которые необходимо вернуть
+        /// </summary>
+        /// <remarks>
+        /// Этот параметр учитывается только при IsExtended = 1
+        /// </remarks>
+        IEnumerable<string> Fields { get; set; } // TODO: Возможно стоит сделать отдельный тип
+    }
+}
