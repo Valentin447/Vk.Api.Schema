1f26b491f9c921895d1ee6ca86009cf4b7c6ca3a
Valentin447 <valentin@myttk.ru>
2017-10-30 16:27:26 +0700

    Добавил интерфейс для представления параметров запроса,который позволяет пожаловаться на комментарий к записи

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallReportCommentParameters.cs
@@ -0,0 +1,33 @@
+﻿using Vk.Api.Schema.Enums;
+
+namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.reportComment <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.reportComment"/>
+    /// </summary>
+    public interface IWallReportCommentParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, которому принадлежит комментарий
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор комментария
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int CommentId { get; set; }
+
+        /// <summary>
+        /// Причина жалобы
+        /// </summary>
+        ReportContentType Reason { get; set; }
+    }
+}
