1453126e767e871588ae59e93ac20c174a0c5fc7
Valentin447 <valentin@myttk.ru>
2017-10-30 16:58:51 +0700

    Добавил интерфейс для представления параметров запроса, для восстанавления удаленного комментария к записи на стене

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallRestoreCommentParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.restoreComment <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.restoreComment"/>
+    /// </summary>
+    public interface IWallRestoreCommentParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, на стене которого находится комментарий к записи
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию идентификатор текущего пользователя
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор комментария
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int CommentId { get; set; }
+    }
+}
