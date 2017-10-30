8d708b7b5910a463e27f1ae18108d12057dde6bf
Valentin447 <valentin@myttk.ru>
2017-10-30 17:15:58 +0700

    Добавил интерфейс для представления параметров запроса, который отменяет закрепление записи на стене

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallUnpinParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.unpin <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.unpin"/>
+    /// </summary>
+    public interface IWallUnpinParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, на стене которого находится запись
+        /// </summary>
+        /// <remarks>
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор записи на стене
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int PostId { get; set; }
+    }
+}
