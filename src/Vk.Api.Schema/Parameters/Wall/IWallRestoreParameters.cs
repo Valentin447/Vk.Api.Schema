ab603de7ff613b10be0eb99ba5b75160fdd540c4
Valentin447 <valentin@myttk.ru>
2017-10-30 16:50:27 +0700

    Добавил интерфейс для представления параметров запроса, восстанавливающего удаленную запись на стене

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallRestoreParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.restore <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.restore"/>
+    /// </summary>
+    public interface IWallRestoreParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, на стене которого находилась удаленная запись
+        /// </summary>
+        /// <remarks>
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор записи
+        /// </summary>
+        int? PostId { get; set; }
+    }
+}
