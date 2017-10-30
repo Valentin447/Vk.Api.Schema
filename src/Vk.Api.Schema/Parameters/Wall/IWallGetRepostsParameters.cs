6dfca2130aaa1d92ced906bd25a88cb0bd69f2c6
Valentin447 <valentin@myttk.ru>
2017-10-30 15:15:48 +0700

    Добавил интерфейс для представления параметров запроса, позволяющего получить список репостов заданной записи

---
+++ src/Vk.Api.Schema/Parameters/Wall/IWallGetRepostsParameters.cs
@@ -0,0 +1,36 @@
+﻿namespace Vk.Api.Schema.Parameters.Wall
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для wall.getReposts <para/>
+    /// Документация: <see href="https://vk.com/dev/wall.getReposts"/>
+    /// </summary>
+    public interface IWallGetRepostsParameters
+    {
+        /// <summary>
+        /// Идентификатор пользователя или сообщества, со стены которого необходимо получить записи
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию идентификатор текущего пользователя
+        /// Идентификатор сообщества в параметре OwnerId необходимо указывать со знаком "-"
+        /// </remarks>
+        int? OwnerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор записи на стене
+        /// </summary>
+        int? PostId { get; set; }
+
+        /// <summary>
+        /// Смещение, необходимое для выборки определенного подмножества записей
+        /// </summary>
+        int? Offset { get; set; }
+
+        /// <summary>
+        /// Количество записей, которое необходимо получить
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию 20, максимальное значение 1000
+        /// </remarks>
+        int? Count { get; set; }
+    }
+}
