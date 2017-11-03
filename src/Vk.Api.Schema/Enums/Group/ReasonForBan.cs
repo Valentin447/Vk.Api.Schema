6f49e7cd96cf38669a3d73b4c94117f779292c17
Valentin447 <valentin@myttk.ru>
2017-11-03 16:28:51 +0700

    Добавил перечисление и интерфейс для представления параметров запроса, добавляющего пользователя в черный список сообщества

---
+++ src/Vk.Api.Schema/Enums/Group/ReasonForBan.cs
@@ -0,0 +1,33 @@
+﻿namespace Vk.Api.Schema.Enums.Group
+{
+    /// <summary>
+    /// Причина бана
+    /// </summary>
+    public enum ReasonForBan
+    {
+        /// <summary>
+        /// Другое (по умолчанию)
+        /// </summary>
+        Other,
+
+        /// <summary>
+        /// Спам
+        /// </summary>
+        Spam,
+
+        /// <summary>
+        /// Оскорбление участников
+        /// </summary>
+        Insult,
+
+        /// <summary>
+        /// Нецензурные выражения
+        /// </summary>
+        Obscene,
+
+        /// <summary>
+        /// Сообщения не по теме
+        /// </summary>
+        OffTopic
+    }
+}
---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupBanUserParameters.cs
@@ -0,0 +1,55 @@
+﻿using Vk.Api.Schema.Enums.Group;
+
+namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.banUser <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.banUser"/>
+    /// </summary>
+    public interface IGroupBanUserParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя, которого нужно добавить в черный список
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int UserId { get; set; }
+
+        /// <summary>
+        /// Дата завершения срока действия бана в формате unixtime
+        /// </summary>
+        /// <remarks>
+        /// Максимальный возможный срок окончания бана, который можно указать, — один год с его начала.
+        /// Если параметр не указан, пользователь будет заблокирован навсегда
+        /// </remarks>
+        int? EndDate { get; set; } // TODO: DateTime?
+
+        /// <summary>
+        /// Причина бана
+        /// </summary>
+        ReasonForBan Reason { get; set; }
+
+        /// <summary>
+        /// Текст комментария к бану
+        /// </summary>
+        string Comment { get; set; }
+
+        /// <summary>
+        /// Текст комментария будет отображаться пользователю
+        /// </summary>
+        /// <remarks>
+        /// 0 – текст комментария не доступен пользователю
+        /// По умолчанию: 0
+        /// </remarks>
+        bool? CommentIsVisible { get; set; }
+    }
+}
