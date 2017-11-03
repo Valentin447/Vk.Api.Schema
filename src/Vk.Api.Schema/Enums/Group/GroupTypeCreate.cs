71926576d1c7adcbf4b2567ad67930d5f4352964
Valentin447 <valentin@myttk.ru>
2017-11-03 21:06:00 +0700

    Добавил перечисления и интрефейс для представления параметров запроса, создающий новое сообщество

---
+++ src/Vk.Api.Schema/Enums/Group/GroupTypeCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Vk.Api.Schema.Enums.Group
+{
+    /// <summary>
+    /// Тип создаваемого сообщества
+    /// </summary>
+    public enum GroupTypeCreate // Единственное отличие от перечисления GroupType, в значении Public
+    {
+        /// <summary>
+        /// Группа
+        /// </summary>
+        [Description("group")]
+        Group,
+
+        /// <summary>
+        /// Публичная страница
+        /// </summary>
+        [Description("public")]
+        Public,
+
+        /// <summary>
+        /// Мероприятие
+        /// </summary>
+        [Description("event")]
+        Event
+    }
+}
