2510292d83760b8975dfa7bc6d787e19f79ea6f9
Valentin447 <valentin@myttk.ru>
2017-11-03 21:15:59 +0700

    Добавил интерфейс для представления параметров запроса, который удаляет сервер для Callback API из сообщества

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupDeleteCallbackServerParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.deleteCallbackServer <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.deleteCallbackServer"/>
+    /// </summary>
+    public interface IGroupDeleteCallbackServerParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор сервера, который нужно удалить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int ServerId { get; set; }
+    }
+}
