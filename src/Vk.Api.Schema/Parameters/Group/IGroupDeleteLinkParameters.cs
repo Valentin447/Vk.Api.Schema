e33d127bd10c74b6398dbcb6744274f3a4a7cecc
Valentin447 <valentin@myttk.ru>
2017-11-03 21:22:21 +0700

    Добавил интерфейс для представления параметров запроса, который позволяет удалить ссылки из сообщества

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupDeleteLinkParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.deleteLink <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.deleteLink"/>
+    /// </summary>
+    public interface IGroupDeleteLinkParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества, ссылки которого нужно удалить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор ссылки, которую необходимо удалить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int LinkId { get; set; }
+    }
+}
