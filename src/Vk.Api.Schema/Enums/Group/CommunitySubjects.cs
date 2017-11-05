7694c2f62c9193c8a1d3f63033438f6771fdb4cf
Valentin447 <valentin@myttk.ru>
2017-11-05 15:04:28 +0700

    Добавил 3 перечисление и интерфейс для представления параметров запроса, который редактирует сообщество

---
+++ src/Vk.Api.Schema/Enums/Group/CommunitySubjects.cs
@@ -0,0 +1,218 @@
+﻿namespace Vk.Api.Schema.Enums.Group
+{
+    /// <summary>
+    /// Тематика сообщества
+    /// </summary>
+    public enum CommunitySubjects
+    {
+        /// <summary>
+        /// Авто/мото
+        /// </summary>
+        Moto = 1,
+
+        /// <summary>
+        /// Активный отдых
+        /// </summary>
+        Leisure,
+
+        /// <summary>
+        /// Бизнес
+        /// </summary>
+        Business,
+
+        /// <summary>
+        /// Домашние животные
+        /// </summary>
+        Pets,
+
+        /// <summary>
+        /// Здоровье
+        /// </summary>
+        Health,
+
+        /// <summary>
+        /// Знакомство и общение
+        /// </summary>
+        Communication,
+
+        /// <summary>
+        /// Игры
+        /// </summary>
+        Games,
+
+        /// <summary>
+        /// ИТ (компьютеры и софт)
+        /// </summary>
+        IT,
+
+        /// <summary>
+        /// Кино
+        /// </summary>
+        Film,
+
+        /// <summary>
+        /// Красота и мода
+        /// </summary>
+        Beauty,
+
+        /// <summary>
+        /// Кулинария
+        /// </summary>
+        Cookery,
+
+        /// <summary>
+        /// Культура и искусство
+        /// </summary>
+        Culture,
+
+        /// <summary>
+        /// Литература
+        /// </summary>
+        Literature,
+
+        /// <summary>
+        /// Мобильная связь и интернет
+        /// </summary>
+        Internet,
+
+        /// <summary>
+        /// Музыка
+        /// </summary>
+        Music,
+
+        /// <summary>
+        /// Наука и техника
+        /// </summary>
+        Science,
+
+        /// <summary>
+        /// Недвижимость
+        /// </summary>
+        RealEstate,
+
+        /// <summary>
+        /// Новости и СМИ
+        /// </summary>
+        News,
+
+        /// <summary>
+        /// Безопасность
+        /// </summary>
+        Security,
+
+        /// <summary>
+        /// Образование
+        /// </summary>
+        Education,
+
+        /// <summary>
+        /// Обустройство и ремонт
+        /// </summary>
+        Repair,
+
+        /// <summary>
+        /// Политика
+        /// </summary>
+        Politics,
+
+        /// <summary>
+        /// Продукты питания
+        /// </summary>
+        Food,
+
+        /// <summary>
+        /// Промышленность
+        /// </summary>
+        Industry,
+
+        /// <summary>
+        /// Путешествия
+        /// </summary>
+        Travels,
+
+        /// <summary>
+        /// Работа
+        /// </summary>
+        Job,
+
+        /// <summary>
+        /// Развлечения
+        /// </summary>
+        Entertainment,
+
+        /// <summary>
+        /// Религия
+        /// </summary>
+        Religion,
+
+        /// <summary>
+        /// Дом и семья
+        /// </summary>
+        Home,
+
+        /// <summary>
+        /// Спорт
+        /// </summary>
+        Sport,
+
+        /// <summary>
+        /// Страхование
+        /// </summary>
+        Insurance,
+
+        /// <summary>
+        /// Телевидение
+        /// </summary>
+        Television,
+
+        /// <summary>
+        /// Товары и услуги
+        /// </summary>
+        Goods,
+
+        /// <summary>
+        /// Увлечения и хобби
+        /// </summary>
+        Hobbies,
+
+        /// <summary>
+        /// Финансы
+        /// </summary>
+        Finance,
+
+        /// <summary>
+        /// Фото
+        /// </summary>
+        Photo,
+
+        /// <summary>
+        /// Эзотерика
+        /// </summary>
+        Esoterics,
+
+        /// <summary>
+        /// Электроника и бытовая техника
+        /// </summary>
+        Electronics,
+
+        /// <summary>
+        /// Эротика
+        /// </summary>
+        Eroticism,
+
+        /// <summary>
+        /// Юмор
+        /// </summary>
+        Humor,
+
+        /// <summary>
+        /// Общество, гуманитарные науки
+        /// </summary>
+        Society,
+
+        /// <summary>
+        /// Дизайн и графика
+        /// </summary>
+        Design
+    }
+}
---
+++ src/Vk.Api.Schema/Enums/Group/GroupEditStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace Vk.Api.Schema.Enums.Group
+{
+    /// <summary>
+    /// Статус доступа элементов сообщества
+    /// </summary>
+    public enum GroupEditStatus
+    {
+        /// <summary>
+        /// Выключена
+        /// </summary>
+        Off,
+
+        /// <summary>
+        /// Открытая
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// Ограниченная (доступно только для групп и событий)
+        /// </summary>
+        Limited,
+
+        /// <summary>
+        /// Закрытая (доступно только для групп и событий)
+        /// </summary>
+        Closed
+    }
+}
