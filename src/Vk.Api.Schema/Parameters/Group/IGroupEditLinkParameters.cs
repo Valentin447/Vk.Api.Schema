86808916ac811be78acd6ee1bcfe36c35913d78d
Valentin447 <valentin@myttk.ru>
2017-11-05 15:22:56 +0700

    Добавил интерфейс для представления параметров запроса, позволяющей редактировать ссылки в сообществе

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupEditLinkParameters.cs
@@ -0,0 +1,30 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.editLink <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.editLink"/>
+    /// </summary>
+    public interface IGroupEditLinkParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества, в которое добавляется ссылка
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор ссылки
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int LinkId { get; set; }
+
+        /// <summary>
+        /// Новый текст описания для ссылки
+        /// </summary>
+        string Text { get; set; }
+    }
+}
