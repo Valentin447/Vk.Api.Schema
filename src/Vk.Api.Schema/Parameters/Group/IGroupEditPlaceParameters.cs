8abad28b538cb8f1c71ffa0b53f7583d0f452489
Valentin447 <valentin@myttk.ru>
2017-11-05 15:48:45 +0700

    Добавил интерфейс для представления параметров запроса, который позволяюет редактировать информацию о месте группы

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupEditPlaceParameters.cs
@@ -0,0 +1,53 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.editPlace <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.editPlace"/>
+    /// </summary>
+    public interface IGroupEditPlaceParameters
+    {
+        /// <summary>
+        /// Идентификатор группы, информацию о месте которой нужно отредактировать
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Название места
+        /// </summary>
+        string Title { get; set; }
+
+        /// <summary>
+        /// Строковой адрес места
+        /// </summary>
+        string Address { get; set; }
+
+        /// <summary>
+        /// Идентификатор страны
+        /// </summary>
+        /// <remarks>
+        /// Список доступных стран можно получить методом database.getCountries
+        /// </remarks>
+        int? CountryId { get; set; }
+
+        /// <summary>
+        /// Идентификатор города
+        /// </summary>
+        /// <remarks>
+        /// Список доступных городов можно получить методом database.getCities
+        /// </remarks>
+        int? CityId { get; set; }
+
+        /// <summary>
+        /// Географическая широта отметки, заданная в градусах (от -90 до 90)
+        /// </summary>
+        double? Latitude { get; set; }
+
+        /// <summary>
+        /// Географическая долгота отметки, заданная в градусах (от -180 до 180)
+        /// </summary>
+        double? Longitude { get; set; }
+    }
+}
