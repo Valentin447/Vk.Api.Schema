ce5647268e1aa31545390d69b6c72a2097851cdc
Valentin447 <valentin@myttk.ru>
2017-11-06 12:10:56 +0700

    Добавил интерфейс для представления параметров запроса, который возвращает список категорий для каталога сообществ

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupGetCatalogInfoParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.getCatalogInfo <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.getCatalogInfo"/>
+    /// </summary>
+    public interface IGroupGetCatalogInfoParameters
+    {
+        /// <summary>
+        /// Вернуть информацию о количестве сообществ в категории и три сообщества для предпросмотра
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию 0, доступен начиная с версии 5.37
+        /// </remarks>
+        bool? IsExtended { get; set; }
+
+        /// <summary>
+        /// Вернуть информацию об подкатегориях
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию 0, доступен начиная с версии 5.37
+        /// </remarks>
+        bool? GetSubcategories { get; set; }
+    }
+}
