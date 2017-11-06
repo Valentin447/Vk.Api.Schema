1fa4684222f71fdfe6efc427f15ccdd44445a599
Valentin447 <valentin@myttk.ru>
2017-11-06 18:39:46 +0700

    Добавил перечисление и интерфейс для представления параметров запроса, который возвращает список участников сообщества

---
+++ src/Vk.Api.Schema/Enums/Filters/GroupMembersFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Vk.Api.Schema.Enums.Filters
+{
+    /// <summary>
+    /// Фильтр списка участников сообщества
+    /// </summary>
+    public enum GroupMembersFilter
+    {
+        /// <summary>
+        /// Будут возвращены только друзья в этом сообществе
+        /// </summary>
+        [Description("friends")]
+        Friends,
+
+        /// <summary>
+        /// Будут возвращены пользователи, которые выбрали «Возможно пойду» (если сообщество относится к мероприятиям)
+        /// </summary>
+        [Description("unsure")]
+        Unsure,
+
+        /// <summary>
+        /// будут возвращены только руководители сообщества (доступно при запросе с передачей access_token от имени администратора сообщества)
+        /// </summary>
+        [Description("managers")]
+        Managers
+    }
+}
---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupGetMembersParameters.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Vk.Api.Schema.Enums.Filters;
+using Vk.Api.Schema.Enums.Group;
+
+namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.getMembers <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.getMembers"/>
+    /// </summary>
+    public interface IGroupGetMembersParameters
+    {
+        /// <summary>
+        /// Идентификатор или короткое имя сообщества
+        /// </summary>
+        string GroupId { get; set; }
+
+        /// <summary>
+        /// Сортировка, с которой необходимо вернуть список участников
+        /// </summary>
+        /// <remarks>
+        /// Сортировка по time_asc и time_desc возможна только при вызове от модератора сообщества 
+        /// По умолчанию id_asc
+        /// </remarks>
+        SortMembers? Sort { get; set; }
+
+        /// <summary>
+        /// Смещение, необходимое для выборки определенного подмножества участников
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию: 0
+        /// </remarks>
+        int? Offset { get; set; }
+
+        /// <summary>
+        /// Количество участников сообщества, информацию о которых необходимо получить
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию 1000, максимальное значение 1000
+        /// </remarks>
+        int? Count { get; set; }
+
+        /// <summary>
+        /// Список дополнительных полей, которые необходимо вернуть
+        /// </summary>
+        IEnumerable<UserField> Fields { get; set; }
+
+        /// <summary>
+        /// Фильтр списка участников сообщества
+        /// </summary>
+        GroupMembersFilter? Filter { get; set; }
+    }
+}
