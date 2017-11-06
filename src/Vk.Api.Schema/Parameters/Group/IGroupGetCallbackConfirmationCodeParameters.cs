9a740c179092694d6976a10e73a65b72c0058ee8
Valentin447 <valentin@myttk.ru>
2017-11-06 11:12:23 +0700

    Добавил интерфейс для представления параметров запроса, который позволяет получить строку, необходимую для подтверждения адреса сервера в Callback API

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupGetCallbackConfirmationCodeParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.getCallbackConfirmationCode <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.getCallbackConfirmationCode"/>
+    /// </summary>
+    public interface IGroupGetCallbackConfirmationCodeParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+    }
+}
