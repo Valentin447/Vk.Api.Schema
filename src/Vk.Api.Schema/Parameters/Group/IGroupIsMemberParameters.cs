c7f102194e4416945daa907b2b6ececd50d7ecae
Valentin447 <valentin@myttk.ru>
2017-11-06 19:10:32 +0700

    Добавил интерфейс для представления параметров запроса, который возвращает информацию о том, является ли пользователь участником сообщества

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupIsMemberParameters.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.isMember <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.isMember"/>
+    /// </summary>
+    public interface IGroupIsMemberParameters
+    {
+        /// <summary>
+        /// Идентификатор или короткое имя сообщества
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        string GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        int? UserId { get; set; }
+
+        /// <summary>
+        /// Идентификаторы пользователей
+        /// </summary>
+        /// <remarks>
+        /// Не более 500
+        /// </remarks>
+        IEnumerable<int> UserIds { get; set; }
+
+        /// <summary>
+        /// Вернуть ответ в расширенной форме
+        /// </summary>
+        /// <remarks>
+        /// По умолчанию — 0
+        /// </remarks>
+        bool? IsExtended { get; set; }
+    }
+}
