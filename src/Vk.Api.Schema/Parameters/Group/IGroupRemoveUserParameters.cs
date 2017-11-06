0381fb441695696d70f8909ff35b9ce54a7e0bc7
Valentin447 <valentin@myttk.ru>
2017-11-06 21:01:03 +0700

    Добавил интерфейс для представления параметров запроса, который позволяет исключить пользователя из группы или отклонить заявку на вступление

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupRemoveUserParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.removeUser <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.removeUser"/>
+    /// </summary>
+    public interface IGroupRemoveUserParameters
+    {
+        /// <summary>
+        /// Идентификатор группы, из которой необходимо исключить пользователя
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя, которого нужно исключить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int UserId { get; set; }
+    }
+}
