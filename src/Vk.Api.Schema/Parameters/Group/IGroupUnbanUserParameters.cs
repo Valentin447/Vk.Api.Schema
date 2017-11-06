a1e56ca5530c464a1cf5857af76b5683af46f367
Valentin447 <valentin@myttk.ru>
2017-11-06 22:32:12 +0700

    Добавил интерфейс для представления параметров запроса, который убирает пользователя из черного списка сообщества

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupUnbanUserParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.unbanUser <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.unbanUser"/>
+    /// </summary>
+    public interface IGroupUnbanUserParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя, которого нужно убрать из черного списка
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int UserId { get; set; }
+    }
+}
