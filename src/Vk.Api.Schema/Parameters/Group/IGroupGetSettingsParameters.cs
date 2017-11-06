172014cd695996d75307f61e255fd32608cddc4d
Valentin447 <valentin@myttk.ru>
2017-11-06 18:55:13 +0700

    Добавил интерфейс для представления параметров запроса, который позволяет получать данные, необходимые для отображения страницы редактирования данных сообщества

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupGetSettingsParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.getSettings <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.getSettings"/>
+    /// </summary>
+    public interface IGroupGetSettingsParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества, данные которого необходимо получить
+        /// </summary>
+        /// <remarks>
+        /// Обязательный параметр
+        /// </remarks>
+        int GroupId { get; set; }
+    }
+}
