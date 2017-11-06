20009a6aaf7c61e1c2f75490e7af596a2d9d85a9
Valentin447 <valentin@myttk.ru>
2017-11-06 20:52:23 +0700

    Добавил интерфейс для представления параметров запроса, который позволяет вступить в группу, публичную страницу, а также подтвердить участие во встрече

---
+++ src/Vk.Api.Schema/Parameters/Group/IGroupJoinParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace Vk.Api.Schema.Parameters.Group
+{
+    /// <summary>
+    /// Интерфейс для представления параметров запроса для groups.join <para/>
+    /// Документация: <see href="https://vk.com/dev/groups.join"/>
+    /// </summary>
+    public interface IGroupJoinParameters
+    {
+        /// <summary>
+        /// Идентификатор сообщества
+        /// </summary>
+        int? GroupId { get; set; }
+
+        /// <summary>
+        /// Опциональный параметр, учитываемый, если GroupId принадлежит встрече
+        /// </summary>
+        /// <remarks>
+        /// 1 — Возможно пойду. 0 — Точно пойду. По умолчанию 0
+        /// </remarks>
+        string NotSure { get; set; } // TODO: По документации - строка, по логике - булл или перечисление. проверить
+    }
+}
